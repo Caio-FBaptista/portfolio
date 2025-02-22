f140f2aebca506d498116f0d8960df525bdf416e
Caio Baptista <caio.sfbaptista@gmail.com>
2025-02-22 15:11:40 -0300

    Configurando menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    let itens = document.getElementById("itens")
+    if (itens.style.display == 'none') {
+        itens.style.display = 'block';
+    } else {
+        itens.style.display = 'none';
+    }
+}
